756f39b2e53db783b766f510712b33b7772e9730
Jbmb4 <3.15602173e+08+Jbmb4@users.noreply.github.com>
2026-08-19 14:02:57 +0000

    Add HTTPS Check User proxy aliases for XHTTP

---
+++ src/routes/check.ts
@@ -0,0 +1,19 @@
+import { FastifyReply, FastifyRequest, RouteOptions } from 'fastify';
+import { CheckUserQuery, handleCheckUserProxy } from '../utils/checkuser-proxy';
+
+export default {
+  url: '/check',
+  method: 'GET',
+  schema: {
+    querystring: {
+      type: 'object',
+      properties: {
+        user: { type: 'string', minLength: 1, maxLength: 128 },
+        username: { type: 'string', minLength: 1, maxLength: 128 },
+      },
+      anyOf: [{ required: ['user'] }, { required: ['username'] }],
+    },
+  },
+  handler: (request: FastifyRequest<{ Querystring: CheckUserQuery }>, reply: FastifyReply) =>
+    handleCheckUserProxy(request, reply),
+} as RouteOptions;
